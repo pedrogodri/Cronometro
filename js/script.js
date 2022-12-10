4df8c6528013aaa3db1038edb0d75ef4607d3232
Pedro Henrique Godri <pedrogodri@gmail.com>
2022-12-10 11:58:44 -0300

    Commit - Projeto Finalizado

--- js/script.js
+++ js/script.js
@@ -8,17 +8,42 @@ var tempo = 1000;
 var cronometro;
 
 function start() {
-    cronometro = setInterval(timer, tempo);
+    cronometro = setInterval(() => { timer();}, tempo);
 }
 
 function pause() {
-
+    clearInterval(cronometro);
 }
 
 function stop() {
+    clearInterval(cronometro);
+    timerNull();
 
+    document.getElementById('counter').innerHTML = '00:00:00';
 }
 
 function timer() {
-    document.getElementById('counter').innerText = 'tESTE'
+
+    ss++;
+
+    if(ss == 60) {
+        ss = 0;
+        mm++;
+
+        if(mm == 60) {
+            mm = 0;
+            hh++;
+        }
+    }
+
+    var format = (hh < 10 ? '0' + hh : hh) + ':' 
+               + (mm < 10 ? '0' + mm : mm) + ':' 
+               + (ss < 10 ? '0' + ss : ss);
+    document.getElementById('counter').innerText = format;
+}
+
+function timerNull() {
+    hh = 0;
+    mm = 0;
+    ss = 0;
 }
